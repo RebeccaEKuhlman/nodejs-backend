4af194f7288076cc910753b09cdd89258940263a
RebeccaEKuhlman <rkuhlman@smu.edu>
2024-02-03 16:25:26 -0600

    Update passwordResetRouter.ts

--- src/routes/passwordResetRouter.ts
+++ src/routes/passwordResetRouter.ts
@@ -11,6 +11,13 @@ router.post('/password-reset-link', async (req, res) => {
   const { email } = req.body;
   // todo: write your code here
   // 1. verify if email is in database
+  const ifMail = await prisma.user.findUnique({
+    where: { email: email },
+  });
+
+  if (!ifMail) {
+    return res.status(400).send({ error: 'Email not found in the database.' });
+  }
   
   const timestamp = Date.now();
   const currentDate = new Date(timestamp);
